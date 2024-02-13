0908fa0209eed99470780f1c9bf6ea98ff51539d
T.Dudchenko <dudchenko.ta@yandex.ru>
2024-02-13 14:53:23 +0500

    mutation

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // 1 Переменные
 // camelCase
 const firstName ='Ivan'
-const lastName = 'Ivanov' // string
-let age = 26 // number
+const age = 26 // number
 const isProgrammer = true // boolean
 
 const __ = 'private'
@@ -11,3 +10,10 @@ const $ = 'some value'
 // const if = 'jhgjh' // err
 const withNum5 = '5'
 // const 5withNum = 5 // err
+
+// Мутирование
+// console.log('Имя человека: ' + firstName + ', а возраст: ' + age)
+// alert('Имя человека: ' + firstName + ', а возраст: ' + age)
+
+// const lastName = prompt('Введите фамилию')
+// alert(firstName + ' ' + lastName)
