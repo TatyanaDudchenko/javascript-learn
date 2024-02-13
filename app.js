c4e445c9d4c716e315dcc4ff12143ccf8645f810
T.Dudchenko <dudchenko.ta@yandex.ru>
2024-02-13 15:24:37 +0500

    operators

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 1 Переменные
 // camelCase
 const firstName ='Ivan'
-const age = 26 // number
+// const age = 26 // number
 const isProgrammer = true // boolean
 
 const __ = 'private'
@@ -11,9 +11,36 @@ const $ = 'some value'
 const withNum5 = '5'
 // const 5withNum = 5 // err
 
-// Мутирование
+// 2 Мутирование
 // console.log('Имя человека: ' + firstName + ', а возраст: ' + age)
 // alert('Имя человека: ' + firstName + ', а возраст: ' + age)
 
 // const lastName = prompt('Введите фамилию')
 // alert(firstName + ' ' + lastName)
+
+// 3 Операторы
+let currentYear = 2020
+const birthYear = 1993
+
+// const age = currentYear + birthYear
+
+const a = 10
+const b = 5
+
+let c = 32
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(++currentYear)
+// console.log(--currentYear)
+// console.log(c)
