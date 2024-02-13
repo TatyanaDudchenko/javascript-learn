7640090731740d90c3b4253c627df1c132f09463
T.Dudchenko <dudchenko.ta@yandex.ru>
2024-02-13 16:19:42 +0500

    data types

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // 1 Переменные
 // camelCase
-const firstName ='Ivan'
+// const firstName ='Ivan'
 // const age = 26 // number
-const isProgrammer = true // boolean
+// const isProgrammer = true // boolean
 
-const __ = 'private'
-const $ = 'some value'
+// const __ = 'private'
+// const $ = 'some value'
 
 // const if = 'jhgjh' // err
-const withNum5 = '5'
+// const withNum5 = '5'
 // const 5withNum = 5 // err
 
 // 2 Мутирование
@@ -19,15 +19,15 @@ const withNum5 = '5'
 // alert(firstName + ' ' + lastName)
 
 // 3 Операторы
-let currentYear = 2020
-const birthYear = 1993
+// let currentYear = 2020
+// const birthYear = 1993
 
 // const age = currentYear + birthYear
 
-const a = 10
-const b = 5
+// const a = 10
+// const b = 5
 
-let c = 32
+// let c = 32
 // c = c + a
 // c = c - a
 // c = c * a
@@ -44,3 +44,14 @@ let c = 32
 // console.log(++currentYear)
 // console.log(--currentYear)
 // console.log(c)
+
+// Типы данных
+const isProgrammer = true
+const name = 'Ivanov'
+const age = 26
+let x
+console.log(typeof isProgrammer)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof x)
+console.log(typeof null)
